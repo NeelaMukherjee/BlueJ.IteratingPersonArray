fe1e63b955f5275f65759beb8b16f6df45dd2326
Neelanjana Mukherjee <neelanjanamukherjee@zipcoders-MacBook-Pro.local>
2019-02-08 23:08:23 -0500

    all the person loops passed

--- PersonHandler.java
+++ PersonHandler.java
@@ -1,10 +1,12 @@
-package com.zipcodewilmington;
+ 
 
 /**
  * Created by leon on 1/24/18.
  */
 public class PersonHandler {
     private final Person[] personArray;
+    
+//Person p=new Person ();
 
     public PersonHandler(Person[] personArray) {
         this.personArray = personArray;
@@ -13,14 +15,18 @@ public class PersonHandler {
     public String whileLoop() {
         String result = "";
         // assume there is a `counter`
+        int counter=0;
+        while (counter< personArray.length){
         // while `counter` is less than length of array
             // begin loop
+result=result+personArray [counter];
 
                 // use `counter` to identify the `current Person` in the array
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
-
+counter++;
             // end loop
+        }
         return result;
     }
 
@@ -33,11 +39,17 @@ public class PersonHandler {
         // identify increment
 
         // use the above clauses to declare for-loop signature
+        
+        for(int i=0;i<personArray.length;i++){
+           
             // begin loop
                 // use `counter` to identify the `current Person` in the array
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
+                
+                result=result+personArray [i];
             // end loop
+        }
 
         return result;
     }
@@ -51,9 +63,15 @@ public class PersonHandler {
 
         // use the above discoveries to declare for-each-loop signature
             // begin loop
+            
+            for(Person person:personArray){
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
+                
+                result=result+person;
             // end loop
+            
+        }
 
         return result;
     }
